1b0c8ab7327f2b3743f9373d5665e4d7976ad2a9
Jon Jagger <jon@jaggersoft.com>
2020-05-23 11:18:16 +0100

    Add comment to cyber-dojo.sh trap handler

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -2,6 +2,9 @@
 trap tidy_up EXIT
 function tidy_up()
 {
+  # cyber-dojo returns text files under /sandbox that are
+  # created/deleted/changed. In here you can remove any
+  # such files you don't want returned to the browser.  
   [ ! -d objs/ ] || rm -rf objs/
 }
 

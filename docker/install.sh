5722293b949c62b6e5a2eb173057adf557d123aa
JonJagger <jon@kosli.com>
2025-07-01 13:39:48 +0100

    Try using install instructions from cpputest repo

--- docker/install.sh
+++ docker/install.sh
@@ -5,14 +5,22 @@ apk add --no-cache autoconf automake git libtool
 
 cd /
 
-wget https://github.com/cpputest/cpputest/releases/download/latest-passing-build/cpputest-latest.tar.gz
-tar -xf ./cpputest-latest.tar.gz
-rm ./cpputest-latest.tar.gz
-mv cpputest-latest cpputest
-
-cd cpputest/
-autoreconf . -i
-./configure
+git clone https://github.com/cpputest/cpputest.git
+cd cpputest
+mkdir cpputest_build
+cd cpputest_build
+autoreconf .. -i
+../configure
 make
+
+#wget https://github.com/cpputest/cpputest/releases/download/latest-passing-build/cpputest-latest.tar.gz
+#tar -xf ./cpputest-latest.tar.gz
+#rm ./cpputest-latest.tar.gz
+#mv cpputest-latest cpputest
+
+#cd cpputest/
+#autoreconf . -i
+#./configure
+#make
 
 apk del git

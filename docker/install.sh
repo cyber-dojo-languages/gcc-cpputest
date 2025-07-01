98df20e3c07bb38583c5a199ea19e24879eec28d
JonJagger <jon@kosli.com>
2025-07-01 14:03:49 +0100

    Delete commented lines in install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -3,24 +3,12 @@ set -Eeu
 
 apk add --no-cache autoconf automake git libtool
 
-cd /
-
 git clone https://github.com/cpputest/cpputest.git
 cd cpputest
 mkdir cpputest_build
 cd cpputest_build
 autoreconf .. -i
 ../configure
-make
-
-#wget https://github.com/cpputest/cpputest/releases/download/latest-passing-build/cpputest-latest.tar.gz
-#tar -xf ./cpputest-latest.tar.gz
-#rm ./cpputest-latest.tar.gz
-#mv cpputest-latest cpputest
-
-#cd cpputest/
-#autoreconf . -i
-#./configure
-#make
+make install
 
 apk del git

395c06b88c0e58a92577207704c7e72394cd4ad7
JonJagger <jon@kosli.com>
2025-07-19 10:52:28 +0100

    Dockerfile: try using an Ubuntu base image

--- docker/install.sh
+++ docker/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -Eeu
 
-apk add --no-cache autoconf automake git libtool
+#apk add --no-cache autoconf automake git libtool
 
-git clone https://github.com/cpputest/cpputest.git
-cd cpputest
-mkdir cpputest_build
-cd cpputest_build
-autoreconf .. -i
-../configure
-make install
-
-apk del git
+#git clone https://github.com/cpputest/cpputest.git
+#cd cpputest
+#mkdir cpputest_build
+#cd cpputest_build
+#autoreconf .. -i
+#../configure
+#make install
+#
+#apk del git

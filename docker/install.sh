d5aff0fd6c027075308efe38ac2a2e00bcd4e88d
JonJagger <jon@kosli.com>
2025-09-06 17:34:50 +0100

    Delete dead comments in install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -6,19 +6,6 @@ apk add --no-cache autoconf automake git libtool
 git clone https://github.com/cpputest/cpputest.git
 cd cpputest
 
-# Ignore failing tests
-#FILE='tests/CppUTest/UtestTest.cpp'
-#head -n 635 "${FILE}" > file.bak
-#echo "IGNORE_TEST(UtestShellPointerArrayTest, firstTestisNotTheFirstTestWithSeed1234)" >> file.bak
-#tail -n +637 "${FILE}" >> file.bak
-#mv file.bak "${FILE}"
-
-#FILE='tests/CppUTest/MemoryLeakWarningTest.cpp'
-#head -n 484 "${FILE}" > file.bak
-#echo "IGNORE_TEST(MemoryLeakWarningThreadSafe, turnOnThreadSafeNewDeleteOverloads)" >> file.bak
-#tail -n +486 "${FILE}" >> file.bak
-#mv file.bak "${FILE}"
-
 autoreconf . -i
 ./configure
 make tdd
